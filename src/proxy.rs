d2cc12b92c32d79d52ce28f1858431f7532ebd5b
Miguel Guarniz <mi9uel9@gmail.com>
2022-11-04 19:58:10 -0400

    Return cached result only when api key is sent in headers

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,8 +1,9 @@
 use crate::Result;
 use moka::future::Cache;
 use rick_and_morty::{character, episode, location};
-use securestore::{KeySource, SecretsManager};
+use securestore::KeySource;
 use std::path::Path;
+use std::sync::Arc;
 use warp::http::StatusCode;
 use warp::reply::Response;
 use warp::{reply, Filter, Reply};
@@ -10,10 +11,12 @@ use warp::{reply, Filter, Reply};
 pub const JSON_API_KEYS_PATH: &str = "api-keys.json";
 pub const SECRET_KEY_PATH: &str = "secret.key";
 
-async fn fetch_location(id: i64, cache: Cache<String, String>) -> Response {
+async fn fetch_location(id: i64, cache: Cache<String, String>, is_key_valid: bool) -> Response {
     let key = format!("location/{}", id);
     if let Some(val) = cache.get(&key) {
-        return Response::new(val.into());
+        if is_key_valid {
+            return Response::new(val.into());
+        }
     }
     let res = match location::get(id).await {
         Ok(res) => res,
@@ -28,10 +31,12 @@ async fn fetch_location(id: i64, cache: Cache<String, String>) -> Response {
     Response::new(val.into())
 }
 
-async fn fetch_location_all(cache: Cache<String, String>) -> Response {
+async fn fetch_location_all(cache: Cache<String, String>, is_key_valid: bool) -> Response {
     let key = String::from("location-all");
     if let Some(val) = cache.get(&key) {
-        return Response::new(val.into());
+        if is_key_valid {
+            return Response::new(val.into());
+        }
     }
     let res = match location::get_all().await {
         Ok(res) => res,
@@ -45,10 +50,12 @@ async fn fetch_location_all(cache: Cache<String, String>) -> Response {
     Response::new(val.into())
 }
 
-async fn fetch_character(id: i64, cache: Cache<String, String>) -> Response {
+async fn fetch_character(id: i64, cache: Cache<String, String>, is_key_valid: bool) -> Response {
     let key = format!("character/{}", id);
     if let Some(val) = cache.get(&key) {
-        return Response::new(val.into());
+        if is_key_valid {
+            return Response::new(val.into());
+        }
     }
     let res = match character::get(id).await {
         Ok(res) => res,
@@ -57,15 +64,17 @@ async fn fetch_character(id: i64, cache: Cache<String, String>) -> Response {
                 .into_response()
         }
     };
-    let val = serde_json::to_string(&res).unwrap();
+    let val = serde_json::to_string(&res).expect("Upstream data to be valid JSON.");
     cache.insert(key, val.clone()).await;
     Response::new(val.into())
 }
 
-async fn fetch_character_all(cache: Cache<String, String>) -> Response {
+async fn fetch_character_all(cache: Cache<String, String>, is_key_valid: bool) -> Response {
     let key = String::from("character-all");
     if let Some(val) = cache.get(&key) {
-        return Response::new(val.into());
+        if is_key_valid {
+            return Response::new(val.into());
+        }
     }
     let res = match character::get_all().await {
         Ok(res) => res,
@@ -74,15 +83,17 @@ async fn fetch_character_all(cache: Cache<String, String>) -> Response {
                 .into_response()
         }
     };
-    let val = serde_json::to_string(&res).unwrap();
+    let val = serde_json::to_string(&res).expect("Upstream data to be valid JSON.");
     cache.insert(key, val.clone()).await;
     Response::new(val.into())
 }
 
-async fn fetch_episode(id: i64, cache: Cache<String, String>) -> Response {
+async fn fetch_episode(id: i64, cache: Cache<String, String>, is_key_valid: bool) -> Response {
     let key = format!("episode/{}", id);
     if let Some(val) = cache.get(&key) {
-        return Response::new(val.into());
+        if is_key_valid {
+            return Response::new(val.into());
+        }
     }
     let res = match episode::get(id).await {
         Ok(res) => res,
@@ -91,15 +102,17 @@ async fn fetch_episode(id: i64, cache: Cache<String, String>) -> Response {
                 .into_response()
         }
     };
-    let val = serde_json::to_string(&res).unwrap();
+    let val = serde_json::to_string(&res).expect("Upstream data to be valid JSON.");
     cache.insert(key, val.clone()).await;
     Response::new(val.into())
 }
 
-async fn fetch_episode_all(cache: Cache<String, String>) -> Response {
+async fn fetch_episode_all(cache: Cache<String, String>, is_key_valid: bool) -> Response {
     let key = String::from("episode-all");
     if let Some(val) = cache.get(&key) {
-        return Response::new(val.into());
+        if is_key_valid {
+            return Response::new(val.into());
+        }
     }
     let res = match episode::get_all().await {
         Ok(res) => res,
@@ -108,9 +121,28 @@ async fn fetch_episode_all(cache: Cache<String, String>) -> Response {
                 .into_response()
         }
     };
-    let val = serde_json::to_string(&res).unwrap();
+    let val = serde_json::to_string(&res).expect("Upstream data to be valid JSON.");
     cache.insert(key, val.clone()).await;
     Response::new(val.into())
+}
+
+#[derive(Clone)]
+struct SecretsManager(Arc<securestore::SecretsManager>);
+
+impl SecretsManager {
+    fn is_key_valid(&self, api_key: Option<String>) -> bool {
+        match api_key {
+            Some(key) => {
+                if let Some((keyword, in_key)) = key.split_once('-') {
+                    if let Ok(k) = self.0.get(keyword) {
+                        return k.as_str() == in_key;
+                    }
+                }
+                false
+            }
+            None => false,
+        }
+    }
 }
 
 pub struct Proxy {
@@ -120,53 +152,87 @@ pub struct Proxy {
 impl Proxy {
     pub fn new() -> Self {
         Self {
-            manager: SecretsManager::load(
-                Path::new(JSON_API_KEYS_PATH),
-                KeySource::Path(Path::new(SECRET_KEY_PATH)),
-            )
-            .expect("Api key JSON file and secret key to exist."),
+            manager: SecretsManager(Arc::new(
+                securestore::SecretsManager::load(
+                    Path::new(JSON_API_KEYS_PATH),
+                    KeySource::Path(Path::new(SECRET_KEY_PATH)),
+                )
+                .expect("Api key JSON file and secret key to exist."),
+            )),
         }
     }
 
     pub async fn run(self) -> Result<()> {
         let cache = Cache::new(1000);
 
-        // Cloning the cache is cheap according to moka docs.
+        let auth = warp::filters::header::optional::<String>("authorization");
+
+        // Cloning the cache and manager are cheap because both are wrapped in Arc.
         let cache_location = cache.clone();
-        let location_all = warp::path!("location").then(move || {
-            let cache = cache_location.clone();
-            async move { fetch_location_all(cache).await }
-        });
+        let manager = self.manager.clone();
+
+        let location_all = auth
+            .map(move |api_key| manager.is_key_valid(api_key))
+            .and(warp::path!("location"))
+            .then(move |key_is_valid| {
+                let cache = cache_location.clone();
+                async move { fetch_location_all(cache, key_is_valid).await }
+            });
 
         let cache_location = cache.clone();
-        let location_id = warp::path!("location" / i64).then(move |id: i64| {
-            let cache = cache_location.clone();
-            async move { fetch_location(id, cache).await }
-        });
+        let manager = self.manager.clone();
+
+        let location_id = auth
+            .map(move |api_key| manager.is_key_valid(api_key))
+            .and(warp::path!("location" / i64))
+            .then(move |key_is_valid, id| {
+                let cache = cache_location.clone();
+                async move { fetch_location(id, cache, key_is_valid).await }
+            });
 
         let cache_character = cache.clone();
-        let character_all = warp::path!("character").then(move || {
-            let cache = cache_character.clone();
-            async move { fetch_character_all(cache).await }
-        });
+        let manager = self.manager.clone();
+
+        let character_all = auth
+            .map(move |api_key| manager.is_key_valid(api_key))
+            .and(warp::path!("character"))
+            .then(move |key_is_valid| {
+                let cache = cache_character.clone();
+                async move { fetch_character_all(cache, key_is_valid).await }
+            });
 
         let cache_character = cache.clone();
-        let character_id = warp::path!("character" / i64).then(move |id: i64| {
-            let cache = cache_character.clone();
-            async move { fetch_character(id, cache).await }
-        });
+        let manager = self.manager.clone();
+
+        let character_id = auth
+            .map(move |api_key| manager.is_key_valid(api_key))
+            .and(warp::path!("character" / i64))
+            .then(move |key_is_valid, id| {
+                let cache = cache_character.clone();
+                async move { fetch_character(id, cache, key_is_valid).await }
+            });
 
         let cache_episode = cache.clone();
-        let episode_all = warp::path!("episode").then(move || {
-            let cache = cache_episode.clone();
-            async move { fetch_episode_all(cache).await }
-        });
+        let manager = self.manager.clone();
+
+        let episode_all = auth
+            .map(move |api_key| manager.is_key_valid(api_key))
+            .and(warp::path!("episode"))
+            .then(move |key_is_valid| {
+                let cache = cache_episode.clone();
+                async move { fetch_episode_all(cache, key_is_valid).await }
+            });
 
         let cache_episode = cache.clone();
-        let episode_id = warp::path!("episode" / i64).then(move |id: i64| {
-            let cache = cache_episode.clone();
-            async move { fetch_episode(id, cache).await }
-        });
+        let manager = self.manager.clone();
+
+        let episode_id = auth
+            .map(move |api_key| manager.is_key_valid(api_key))
+            .and(warp::path!("episode" / i64))
+            .then(move |key_is_valid, id| {
+                let cache = cache_episode.clone();
+                async move { fetch_episode(id, cache, key_is_valid).await }
+            });
 
         let routes = character_id
             .or(character_all)
