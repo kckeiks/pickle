9b1be0dfccd72412650ce4b5c9742e39ec0f4894
Miguel Guarniz <mi9uel9@gmail.com>
2022-11-04 19:01:38 -0400

    Add async-compatible cache

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,40 +1,116 @@
 use crate::Result;
+use moka::future::Cache;
 use rick_and_morty::{character, episode, location};
 use securestore::{KeySource, SecretsManager};
 use std::path::Path;
-use warp::{Filter, Rejection};
+use warp::http::StatusCode;
+use warp::reply::Response;
+use warp::{reply, Filter, Reply};
 
 pub const JSON_API_KEYS_PATH: &str = "api-keys.json";
 pub const SECRET_KEY_PATH: &str = "secret.key";
 
-async fn fetch_location(id: i64) -> std::result::Result<String, Rejection> {
-    let res = location::get(id).await.unwrap();
-    Ok::<String, Rejection>(serde_json::to_string(&res).unwrap())
+async fn fetch_location(id: i64, cache: Cache<String, String>) -> Response {
+    let key = format!("location/{}", id);
+    if let Some(val) = cache.get(&key) {
+        return Response::new(val.into());
+    }
+    let res = match location::get(id).await {
+        Ok(res) => res,
+        Err(_) => {
+            return reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR)
+                .into_response()
+        }
+    };
+
+    let val = serde_json::to_string(&res).expect("Upstream data to be valid JSON.");
+    cache.insert(key, val.clone()).await;
+    Response::new(val.into())
 }
 
-async fn fetch_location_all() -> std::result::Result<String, Rejection> {
-    let res = location::get_all().await.unwrap();
-    Ok::<String, Rejection>(serde_json::to_string(&res).unwrap())
+async fn fetch_location_all(cache: Cache<String, String>) -> Response {
+    let key = String::from("location-all");
+    if let Some(val) = cache.get(&key) {
+        return Response::new(val.into());
+    }
+    let res = match location::get_all().await {
+        Ok(res) => res,
+        Err(_) => {
+            return reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR)
+                .into_response()
+        }
+    };
+    let val = serde_json::to_string(&res).expect("Upstream data to be valid JSON.");
+    cache.insert(key, val.clone()).await;
+    Response::new(val.into())
 }
 
-async fn fetch_character(id: i64) -> std::result::Result<String, Rejection> {
-    let res = character::get(id).await.unwrap();
-    Ok::<String, Rejection>(serde_json::to_string(&res).unwrap())
+async fn fetch_character(id: i64, cache: Cache<String, String>) -> Response {
+    let key = format!("character/{}", id);
+    if let Some(val) = cache.get(&key) {
+        return Response::new(val.into());
+    }
+    let res = match character::get(id).await {
+        Ok(res) => res,
+        Err(_) => {
+            return reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR)
+                .into_response()
+        }
+    };
+    let val = serde_json::to_string(&res).unwrap();
+    cache.insert(key, val.clone()).await;
+    Response::new(val.into())
 }
 
-async fn fetch_character_all() -> std::result::Result<String, Rejection> {
-    let res = character::get_all().await.unwrap();
-    Ok::<String, Rejection>(serde_json::to_string(&res).unwrap())
+async fn fetch_character_all(cache: Cache<String, String>) -> Response {
+    let key = String::from("character-all");
+    if let Some(val) = cache.get(&key) {
+        return Response::new(val.into());
+    }
+    let res = match character::get_all().await {
+        Ok(res) => res,
+        Err(_) => {
+            return reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR)
+                .into_response()
+        }
+    };
+    let val = serde_json::to_string(&res).unwrap();
+    cache.insert(key, val.clone()).await;
+    Response::new(val.into())
 }
 
-async fn fetch_episode(id: i64) -> std::result::Result<String, Rejection> {
-    let res = episode::get(id).await.unwrap();
-    Ok::<String, Rejection>(serde_json::to_string(&res).unwrap())
+async fn fetch_episode(id: i64, cache: Cache<String, String>) -> Response {
+    let key = format!("episode/{}", id);
+    if let Some(val) = cache.get(&key) {
+        return Response::new(val.into());
+    }
+    let res = match episode::get(id).await {
+        Ok(res) => res,
+        Err(_) => {
+            return reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR)
+                .into_response()
+        }
+    };
+    let val = serde_json::to_string(&res).unwrap();
+    cache.insert(key, val.clone()).await;
+    Response::new(val.into())
 }
 
-async fn fetch_episode_all() -> std::result::Result<String, Rejection> {
-    let res = episode::get_all().await.unwrap();
-    Ok::<String, Rejection>(serde_json::to_string(&res).unwrap())
+async fn fetch_episode_all(cache: Cache<String, String>) -> Response {
+    let key = String::from("episode-all");
+    if let Some(val) = cache.get(&key) {
+        return Response::new(val.into());
+    }
+    let res = match episode::get_all().await {
+        Ok(res) => res,
+        Err(_) => {
+            return reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR)
+                .into_response()
+        }
+    };
+    let val = serde_json::to_string(&res).unwrap();
+    cache.insert(key, val.clone()).await;
+    Response::new(val.into())
 }
 
 pub struct Proxy {
@@ -53,12 +129,44 @@ impl Proxy {
     }
 
     pub async fn run(self) -> Result<()> {
-        let location_all = warp::path!("location").and_then(fetch_location_all);
-        let location_id = warp::path!("location" / i64).and_then(fetch_location);
-        let character_all = warp::path!("character").and_then(fetch_character_all);
-        let character_id = warp::path!("character" / i64).and_then(fetch_character);
-        let episode_all = warp::path!("episode").and_then(fetch_episode_all);
-        let episode_id = warp::path!("episode" / i64).and_then(fetch_episode);
+        let cache = Cache::new(1000);
+
+        // Cloning the cache is cheap according to moka docs.
+        let cache_location = cache.clone();
+        let location_all = warp::path!("location").then(move || {
+            let cache = cache_location.clone();
+            async move { fetch_location_all(cache).await }
+        });
+
+        let cache_location = cache.clone();
+        let location_id = warp::path!("location" / i64).then(move |id: i64| {
+            let cache = cache_location.clone();
+            async move { fetch_location(id, cache).await }
+        });
+
+        let cache_character = cache.clone();
+        let character_all = warp::path!("character").then(move || {
+            let cache = cache_character.clone();
+            async move { fetch_character_all(cache).await }
+        });
+
+        let cache_character = cache.clone();
+        let character_id = warp::path!("character" / i64).then(move |id: i64| {
+            let cache = cache_character.clone();
+            async move { fetch_character(id, cache).await }
+        });
+
+        let cache_episode = cache.clone();
+        let episode_all = warp::path!("episode").then(move || {
+            let cache = cache_episode.clone();
+            async move { fetch_episode_all(cache).await }
+        });
+
+        let cache_episode = cache.clone();
+        let episode_id = warp::path!("episode" / i64).then(move |id: i64| {
+            let cache = cache_episode.clone();
+            async move { fetch_episode(id, cache).await }
+        });
 
         let routes = character_id
             .or(character_all)
